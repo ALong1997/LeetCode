c42582d7b3ff498f1a0547285c60079e74ac6d9c
chenlong <cl1026931058@163.com>
2020-05-13 16:13:18 +0800

    problem28

---
+++ easy/28_ImplementStrStr()/28_test.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question28 struct {
+	para28
+	ans28
+}
+
+type para28 struct {
+	aystack string
+	needle string
+}
+
+type ans28 struct {
+	ans int
+}
+
+func Test_Problem28(t *testing.T) {
+	qs := []question28{{
+		para28: para28{"hello", "ll"},
+		ans28:  ans28{2},
+	}, {
+		para28: para28{"aaaab", "bba"},
+		ans28:  ans28{-1},
+	}}
+
+	fmt.Printf("------------------------Leetcode Problem 13------------------------\n")
+
+	for _, q := range qs {
+		a, p := q.ans28, q.para28
+		fmt.Printf("【input】:%v       【output】:%v       【answer】:%v\n", p, strStr(p.aystack, p.needle), a)
+		// fmt.Printf("【input】:%v       【output】:%v       【answer】:%v\n", p, referenceMyAtoi(p.str), a)
+	}
+	fmt.Printf("\n")
+}
