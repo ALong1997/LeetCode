9a2658c370c98c5c54a465e7bc6c72f47ae2de6a
chenlong <cl1026931058@163.com>
2020-05-13 15:52:45 +0800

    problem26

---
+++ easy/26_RemoveDuplicatesFromSortedArray/26_myAnswer.go
@@ -0,0 +1,27 @@
+/*
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+*/
+package leetcode
+
+/*
+解法: 暴力解
+为了避免消耗时间过多，没有采用 append函数删除数组元素的做法
+
+
+结果: 执行用时 :0 ms 内存消耗 :4.6 MB
+*/
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	ans := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[ans] != nums[i] {
+			ans++
+			nums[ans] = nums[i]
+		}
+	}
+	return ans+1
+}
