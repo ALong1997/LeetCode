d58bb06775a339a73d8d40a2d2623f832b9f4b5b
chenlong <cl1026931058@163.com>
2020-06-02 14:11:46 +0800

    problem43

--- medium/43_MultiplyStrings/43_myAnswer.go
+++ medium/43_MultiplyStrings/43_myAnswer.go
@@ -12,7 +12,7 @@ package leetcode
 /*
 解法: 竖式乘法
 num1 * num2 值的位数必定为 len(num1) + len(num2) 第一位可能是0
-num1[i] * num[j] 值必定在resArr[i+j] resArr[i+j+1]上，i+j+1存储地位
+num1[i] * num[j] 值必定在resArr[i+j] resArr[i+j+1]上，i+j+1存储低位
 
 
 结果: 执行用时 :4 ms 内存消耗 :3 MB
@@ -24,25 +24,25 @@ func multiply(num1 string, num2 string) string {
 		return "0"
 	}
 
-	// num1[i] * num[j] 值必定在resArr[i+j] resArr[i+j+1]上，i+j+1存储地位
+	// num1[i] * num[j] 值必定在resArr[i+j] resArr[i+j+1]上，i+j+1存储低位
 	resArr := make([]int, len(num1) + len(num2))
 	for i := len(num2)-1; i >= 0; i -- {
 		n2 := int(num2[i] - '0')
 		for j := len(num1)-1; j >= 0; j -- {
 			n1 := int(num1[j] - '0')
-			sum := n2 * n1 + resArr[i+j+1]
-			resArr[i+j+1] = sum % 10
-			resArr[i+j] += sum / 10
+			product := n2 * n1 + resArr[i+j+1]
+			resArr[i+j+1] = product % 10
+			resArr[i+j] += product / 10
 		}
 	}
 
-	res := ""
+	ans := ""
 	for k, v := range resArr {
 		if k == 0 && v == 0 {
 			continue
 		}
-		res += string(v + '0')
+		ans += string(v + '0')
 	}
 
-	return res
+	return ans
 }
