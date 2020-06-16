beec9b2f7c248c44d69f0ba0a0ff351f98fe420d
chenlong <cl1026931058@163.com>
2020-06-16 10:34:27 +0800

    problem54

---
+++ medium/54_SpiralMatrix/54_reference.go
@@ -0,0 +1,62 @@
+package leetcode
+
+/*
+解法: 按层模拟
+可以将矩阵看成若干层，首先输出最外层的元素，其次输出次外层的元素，直到输出最内层的元素。
+定义矩阵的第 k 层是到最近边界距离为 k 的所有顶点。例如，下图矩阵最外层元素都是第 1 层，次外层元素都是第 2 层，剩下的元素都是第 3 层。
+[[1, 1, 1, 1, 1, 1, 1],
+ [1, 2, 2, 2, 2, 2, 1],
+ [1, 2, 3, 3, 3, 2, 1],
+ [1, 2, 2, 2, 2, 2, 1],
+ [1, 1, 1, 1, 1, 1, 1]]
+
+对于每层，从左上方开始以顺时针的顺序遍历所有元素。
+假设当前层的左上角位于 (top,left)，右下角位于 (bottom,right)，按照如下顺序遍历当前层的元素。
+1.从左到右遍历上侧元素，依次为 (top,left) 到 (top,right)。
+2.从上到下遍历右侧元素，依次为 (top+1,right) 到 (bottom,right)。
+3.如果 left<right 且 top<bottom，则从右到左遍历下侧元素，依次为 (bottom,right−1) 到 (bottom,left+1)，以及从下到上遍历左侧元素，依次为 (bottom,left) 到 (top+1,left)。
+
+遍历完当前层的元素之后，将 left 和 top 分别增加 1，将 right 和 bottom 分别减少 1，进入下一层继续遍历，直到遍历完所有元素为止。
+
+
+结果: 执行用时 :0 ms 内存消耗 :2.1 MB
+*/
+
+
+func referenceSpiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	var (
+		rows, columns = len(matrix), len(matrix[0])
+		order = make([]int, rows * columns)
+		index = 0
+		left, right, top, bottom = 0, columns - 1, 0, rows - 1
+	)
+
+	for left <= right && top <= bottom {
+		for column := left; column <= right; column++ {
+			order[index] = matrix[top][column]
+			index++
+		}
+		for row := top + 1; row <= bottom; row++ {
+			order[index] = matrix[row][right]
+			index++
+		}
+		if left < right && top < bottom {
+			for column := right - 1; column > left; column-- {
+				order[index] = matrix[bottom][column]
+				index++
+			}
+			for row := bottom; row > top; row-- {
+				order[index] = matrix[row][left]
+				index++
+			}
+		}
+		left++
+		right--
+		top++
+		bottom--
+	}
+	return order
+}
