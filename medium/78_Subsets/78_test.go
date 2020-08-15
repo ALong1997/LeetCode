e4b67f845cfe662e2bd3a40a7b4d0790626eb4e1
chenlong <chenlong@woda.ink>
2020-08-15 13:38:07 +0800

    Problem78

---
+++ medium/78_Subsets/78_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question78 struct {
+	para78
+	ans78
+}
+
+type para78 struct {
+	nums []int
+}
+
+type ans78 struct {
+	ans [][]int
+}
+
+func Test_Problem78(t *testing.T) {
+	qs := []question78{{
+		para78: para78{[]int{1, 2, 3}},
+		ans78:  ans78{[][]int{{}, {1}, {2}, {3}, {2, 3}, {1, 2}, {1, 3}, {1, 2, 3}}},
+	}}
+
+	fmt.Printf("------------------------Leetcode Problem 78------------------------\n")
+
+	for _, q := range qs {
+		a, p := q.ans78, q.para78
+		fmt.Printf("【input】:%v       【output】:%v       【answer】:%v\n", p, subsets(p.nums), a)
+		//fmt.Printf("【input】:%v       【output】:%v       【answer】:%v\n", p, referenceCombine(p.n, p.k), a)
+	}
+	fmt.Printf("\n")
+}
